f56f023a0d5da940c11d53fbea5c48fe7dd7002c
Alex-J-Brown <alexjbrown.astro@gmail.com>
2026-09-09 11:40:37 +0200

    Add reverse Planck function

--- src/planck.rs
+++ src/planck.rs
@@ -47,10 +47,33 @@ pub fn dplanck(wave: f64, temp: f64) -> f64 {
 ///
 /// * `wave`: wavelength in nanometres
 /// * `temp`: temperature in K
+/// 
 #[pyfunction]
 pub fn dlpdlt(wave: f64, temp: f64) -> f64 {
     let fac2: f64 = 1.0e9 * H * C / K;
 
     let exponent: f64 = fac2 / (wave * temp);
     exponent / (1.0 - (-exponent).exp())
+}
+
+///
+/// Inverts the Planck function to calculate the blackbody temperature that
+/// results in the given flux at the supplied wavelength
+/// 
+/// Arguments:
+/// 
+/// * `flux`: Spectral flux density in W/m2/Hz/sr
+/// * `wavelength`: Wavelength in nm
+/// 
+/// Returns:
+/// 
+/// * Blackbody temperature in K
+/// 
+#[pyfunction]
+pub fn reverse_planck(flux: f64, wavelength: f64) -> f64 {
+    let wavelength_metres: f64 = wavelength * 1.0e-9;
+
+    let nat_log: f64 = (((2.0 * H * C) / (flux * wavelength_metres.powi(3))) + 1.0).ln();
+    let blackbody_temperature: f64 = (H * C) / (K * wavelength_metres * nat_log);
+    blackbody_temperature
 }
